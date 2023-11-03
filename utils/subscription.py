101d40e42e65e4523f4e2a57803aba2c5076cf77
Zhu, X. F. <git@zhuxiaofeng.me>
2023-11-03 08:57:32 +0800

    fix: subscription use no proxy.

--- utils/subscription.py
+++ utils/subscription.py
@@ -96,7 +96,7 @@ def decode_vmess(ss_server_str: str):
 def subscription_to_servers(url: str, cache_file: str or None):
     result = list()
     try:
-        resp = requests.get(url)
+        resp = requests.get(url, proxies={"http_proxy": "", "https_proxy": ""})
     except Exception as e:
         raise InternalError("requests.get raises exceptions. " + str(e))
     if not resp.ok:
